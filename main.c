4b8bf3561df25b1be5c23b06823249c73ecf7f1d
momen7amza2525 <momenn7amzaa@gmail.com>
2024-09-15 20:53:55 +0300

    v1.0.0

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Hamada 10
+
+int main()
+
+{
+
+int y = 45;
+int x = Hamada;
+printf("X value =%i\n", x);
+printf("y value =%i\n", y);
+
+return 0;
+
+}
